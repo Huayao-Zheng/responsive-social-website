399c4a2a75800d6120f126cbe83098eef48926ff
lonelybuddy <henryzheng92@gmail.com>
2022-03-27 17:24:58 -0500

    completed

--- main.js
+++ main.js
@@ -68,17 +68,17 @@ function closeThemeModal(e) {
 // =========== font-size ==================
 const fontSizeOptions = document.querySelectorAll('.choose-size span');
 
-fontSizeOptions.forEach((fsOption) => {
-  let fontSizeMap = {
-    'font-size-1': '10px',
-    'font-size-2': '13px',
-    'font-size-3': '16px',
-    'font-size-4': '19px',
-    'font-size-5': '22px',
-  };
+let fontSizesMap = {
+  'font-size-1': '10px',
+  'font-size-2': '13px',
+  'font-size-3': '16px',
+  'font-size-4': '19px',
+  'font-size-5': '22px',
+};
 
+fontSizeOptions.forEach((fsOption) => {
   fsOption.addEventListener('click', () => {
-    const fs = fontSizeMap[fsOption.classList[0]];
+    const fs = fontSizesMap[fsOption.classList[0]];
 
     removeActiveClassFrom(fontSizeOptions);
 
@@ -86,4 +86,77 @@ fontSizeOptions.forEach((fsOption) => {
 
     document.querySelector('html').style.fontSize = fs;
   });
+});
+
+// =========== Primary Colors ==================
+const primaryColors = document.querySelectorAll('.choose-color span');
+
+let primaryColorsMap = {
+  'color-1': 252,
+  'color-2': 2,
+  'color-3': 352,
+  'color-4': 152,
+  'color-5': 202,
+};
+
+primaryColors.forEach((pColor) => {
+  pColor.addEventListener('click', () => {
+    const color = primaryColorsMap[pColor.classList[0]];
+
+    removeActiveClassFrom(primaryColors);
+
+    pColor.classList.add('active');
+
+    document.querySelector(':root').style.setProperty('--primary-color-hue', color);
+  });
+});
+
+// <!-- -----------Background----------- -->
+const bg1 = document.querySelector('.bg-1');
+const bg2 = document.querySelector('.bg-2');
+const bg3 = document.querySelector('.bg-3');
+
+// theme background values
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+// changes background colors
+function changeBG() {
+  const root = document.querySelector(':root');
+  root.style.setProperty('--light-color-lightness', lightColorLightness);
+  root.style.setProperty('--white-color-lightness', whiteColorLightness);
+  root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+bg1.addEventListener('click', () => {
+  bg1.classList.add('active');
+  bg2.classList.remove('active');
+  bg3.classList.remove('active');
+
+  window.location.reload();
+});
+
+bg2.addEventListener('click', () => {
+  lightColorLightness = '15%';
+  whiteColorLightness = '20%';
+  darkColorLightness = '95%';
+
+  bg2.classList.add('active');
+  bg1.classList.remove('active');
+  bg3.classList.remove('active');
+
+  changeBG();
+});
+
+bg3.addEventListener('click', () => {
+  lightColorLightness = '0%';
+  whiteColorLightness = '10%';
+  darkColorLightness = '95%';
+
+  bg3.classList.add('active');
+  bg1.classList.remove('active');
+  bg2.classList.remove('active');
+
+  changeBG();
 });
